77fefe3286df290c8b5f97aa56861da06b79054f
tsnbluemacky <tsnbluemacky@gmail.com>
2024-08-26 15:39:08 +0900

    テストコード最終修正

--- db/migrate/20240826063321_add_details_to_users.rb
+++ db/migrate/20240826063321_add_details_to_users.rb
@@ -1,5 +1,6 @@
 class AddDetailsToUsers < ActiveRecord::Migration[7.0]
   def change
+    add_column :users, :nickname, :string
     add_column :users, :first_name, :string
     add_column :users, :last_name, :string
     add_column :users, :first_name_kana, :string
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_08_23_141659) do
+ActiveRecord::Schema[7.0].define(version: 2024_08_26_063321) do
   create_table "users", charset: "utf8mb3", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
