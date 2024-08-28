37e68b52336ab6a5108fd5a26280a4835fc7df7f
tsnbluemacky <tsnbluemacky@gmail.com>
2024-08-28 14:59:18 +0900

    エラーメッセージ重複修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,26 +1,31 @@
 class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
 
-  belongs_to :user
-  has_one_attached :image
-  has_one :order
+  # 関連付け
+  belongs_to :user            # ユーザーとの関連付け
+  has_one_attached :image     # 画像の添付（Active Storage）
+  has_one :order              # 注文情報との関連付け
 
-  belongs_to :category
-  belongs_to :condition
-  belongs_to :shipping
-  belongs_to :prefecture
-  belongs_to :delivery_time
+  belongs_to :category        # カテゴリー情報との関連付け
+  belongs_to :condition       # 商品の状態情報との関連付け
+  belongs_to :shipping        # 配送料情報との関連付け
+  belongs_to :prefecture      # 発送元の地域情報との関連付け
+  belongs_to :delivery_time   # 発送までの日数情報との関連付け
 
+  # バリデーション
   with_options presence: true do
-    validates :image
-    validates :name
-    validates :text
-    validates :price,
-              numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999,
-                              message: '販売可能価格外です' }
+    validates :image                # 画像の添付が必須
+    validates :name                 # 商品名の入力が必須
+    validates :text                 # 商品の説明の入力が必須
+    validates :price, numericality: {
+      only_integer: true,            # 価格は整数であること
+      greater_than_or_equal_to: 300, # 価格が300円以上であること
+      less_than_or_equal_to: 9_999_999, # 価格が9,999,999円以下であること
+      message: '販売可能価格外です' # エラーメッセージ
+    }
   end
 
-  # ActiveHashで使用するIDが0である場合のバリデーション
+  # ActiveHashのIDが0である場合のバリデーション
   validates :category_id, :condition_id, :shipping_id, :prefecture_id, :delivery_time_id,
-            numericality: { other_than: 0, message: 'を選択してください' }
+            numericality: { other_than: 0, message: 'を選択してください' } # IDが0以外であることをチェック
 end
