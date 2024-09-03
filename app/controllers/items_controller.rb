0022e14e84c21edbec3b61e69abdd3a9b8731331
tsnbluemacky <tsnbluemacky@gmail.com>
2024-09-03 15:05:38 +0900

    不要コードの削除

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -5,11 +5,9 @@ class ItemsController < ApplicationController
 
   def index
     @items = Item.order(created_at: :desc)
-    Rails.logger.info("Item index accessed by #{current_user&.email || 'Guest'}")
   end
 
   def show
-    Rails.logger.info("Item #{@item.id} viewed by #{current_user&.email || 'Guest'}")
   end
 
   def new
@@ -19,20 +17,16 @@ class ItemsController < ApplicationController
   def create
     @item = Item.new(item_params)
     if @item.save
-      Rails.logger.info("Item #{@item.id} created by #{current_user.email}")
       redirect_to @item, notice: '商品が作成されました。'
     else
-      Rails.logger.error("Item creation failed: #{@item.errors.full_messages.join(', ')}")
       render :new, status: :unprocessable_entity
     end
   end
 
   def destroy
     if @item.destroy
-      Rails.logger.info("Item #{@item.id} destroyed by #{current_user.email}")
       redirect_to root_path, notice: '商品を削除しました。'
     else
-      Rails.logger.error("Item deletion failed: #{@item.errors.full_messages.join(', ')}")
       redirect_to edit_item_path(@item), alert: '商品の削除に失敗しました。再試行してください。'
     end
   end
@@ -46,9 +40,7 @@ class ItemsController < ApplicationController
 
   def set_item
     @item = Item.find(params[:id])
-    Rails.logger.info("Item #{@item.id} set for processing")
   rescue ActiveRecord::RecordNotFound
-    Rails.logger.error("Item with id #{params[:id]} not found")
     redirect_to root_path, alert: '指定された商品が見つかりません。'
   end
 
@@ -56,7 +48,6 @@ class ItemsController < ApplicationController
     return if current_user == @item.user
 
     flash[:alert] = 'この操作は許可されていません。'
-    Rails.logger.warn("Unauthorized access attempt by #{current_user.email} on item #{@item.id}")
     redirect_to root_path
   end
 end
