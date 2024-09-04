aa3930e00c2456430fbfa677aece11c2489c64a7
tsnbluemacky <tsnbluemacky@gmail.com>
2024-09-04 22:42:52 +0900

    エラーメッセージ重複修正

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -5,17 +5,21 @@ class OrderAddress
   with_options presence: true do
     validates :postal_code, format: { with: /\A\d{3}-\d{4}\z/, message: 'はハイフンを含めて入力してください' }
     validates :prefecture_id, numericality: { other_than: 0, message: 'を選択してください' }
-    validates :city
-    validates :address
+    validates :city, message: 'は入力必須です'
+    validates :address, message: 'は入力必須です'
     validates :phone_number, format: { with: /\A\d{10,11}\z/, message: 'は10桁以上11桁以内の半角数値で入力してください' }
-    validates :token
-    validates :user_id
-    validates :item_id
+    validates :token, message: 'クレジットカード情報を入力してください'
+    validates :user_id, :item_id
   end
 
   def save
-    order = Order.create(user_id:, item_id:)
-    Address.create(postal_code:, prefecture_id:, city:, address:, building:,
-                   phone_number:, order_id: order.id)
+    ActiveRecord::Base.transaction do
+      order = Order.create!(user_id:, item_id:)
+      Address.create!(postal_code:, prefecture_id:, city:, address:, building:,
+                      phone_number:, order_id: order.id)
+    end
+  rescue ActiveRecord::RecordInvalid => e
+    errors.add(:base, "購入処理に失敗しました: #{e.message}")
+    false
   end
 end
